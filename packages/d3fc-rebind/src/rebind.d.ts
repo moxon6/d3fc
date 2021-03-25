faf28914eadb78a493694ca75a2c30aa9e8903b9
Martin Moxon <moxon6@googlemail.com>
2021-03-25 11:21:08 +0000

    Possible working type defintion for rebind - needs tests

--- packages/d3fc-rebind/src/rebind.d.ts
+++ packages/d3fc-rebind/src/rebind.d.ts
@@ -1,1 +1,18 @@
-export default function rebind<TTarget, TSource, TName extends keyof TSource>(target: TTarget, source: TSource, name: TName): TTarget & Pick<TSource, TName>
+type ReplaceTypeIfExistsWith<TInput, TRemove, TReplaceWith> = TInput extends TRemove
+    ? Exclude<TInput, TRemove> | TReplaceWith
+    : TInput
+
+// TODO: There's probably a builtin for this somewhere?
+type ArrowFunction = (...args: any[]) => any
+
+type ReplaceReturnTypeIfExists<TInputFunction extends ArrowFunction, TRemove, TReplaceWith> = (...args: Parameters<TInputFunction>)
+    => ReplaceTypeIfExistsWith<ReturnType<TInputFunction>, TRemove, TReplaceWith>
+
+type PickMethodsAndReplaceReturnType<TTarget, TSource, TMethodNames extends keyof TSource> = {
+    [P in TMethodNames]: TSource[P] extends ArrowFunction
+    ? ReplaceReturnTypeIfExists<TSource[P], TSource, TTarget>
+    : TSource[P]
+}
+
+export default function rebind<TTarget, TSource, TName extends keyof TSource>(target: TTarget, source: TSource, ...names: TName[]):
+    Omit<TTarget, TName> & PickMethodsAndReplaceReturnType<TTarget, TSource, TName>;

93c44ded23721b1e40911a613ef2345c721ec148
Martin Moxon <moxon6@googlemail.com>
2021-04-16 09:38:16 +0100

    Update test comments casing

--- packages/d3fc-chart/test/canvas/cartesian.test-d.ts
+++ packages/d3fc-chart/test/canvas/cartesian.test-d.ts
@@ -5,29 +5,29 @@ import { CartesianBaseChart } from '../../src/cartesianBase';
 import { expectType } from 'tsd';
 import { ScaleIdentity, ScaleLinear, scaleTime, scaleLinear } from 'd3';
 
-// can accept zero scale parameters
+// Can accept zero scale parameters
 expectType<CartesianBaseChart<ScaleIdentity, ScaleIdentity>>(chartCanvasCartesian());
 
-// can accept one scale parameter
+// Can accept one scale parameter
 expectType<CartesianBaseChart<ScaleLinear<number, number, never>, ScaleIdentity>>(chartCanvasCartesian(d3.scaleLinear()));
 
 expectType<CartesianBaseChart<d3.ScaleIdentity<never>, d3.ScaleLinear<number, number, never>>>(chartCanvasCartesian(undefined, d3.scaleLinear()));
 
-// can accept two scale parameters
+// Can accept two scale parameters
 expectType<CartesianBaseChart<d3.ScaleLinear<number, number, never>, d3.ScaleTime<number, number, never>>>(chartCanvasCartesian(d3.scaleLinear(), scaleTime()));
 
-// can accept a configuration object with one scale
+// Can accept a configuration object with one scale
 expectType<CartesianBaseChart<d3.ScaleIdentity, d3.ScaleLinear<number, number, never>>>(chartCanvasCartesian({
     yScale: scaleLinear()
 }));
 
-// can accept a configuration object with both scales
+// Can accept a configuration object with both scales
 expectType<CartesianBaseChart<d3.ScaleLinear<number, number, never>, d3.ScaleLinear<number, number, never>>>(chartCanvasCartesian({
     xScale: scaleLinear(),
     yScale: scaleLinear()
 }));
 
-// can accept a configuration object with optional xaxis, yaxis parameters
+// Can accept a configuration object with optional xaxis, yaxis parameters
 expectType<CartesianBaseChart<d3.ScaleLinear<number, number, never>, d3.ScaleLinear<number, number, never>>>(chartCanvasCartesian({
     xScale: scaleLinear(),
     yScale: scaleLinear(),
@@ -44,12 +44,12 @@ expectType<CartesianBaseChart<d3.ScaleLinear<number, number, never>, d3.ScaleLin
 
 const chart = chartCanvasCartesian(d3.scaleLinear(), d3.scaleLinear());
 
-// has rebound scale methods as any-typed methods
+// Has rebound scale methods as any-typed methods
 expectType<(...args: any[]) => any>(chart.xInterpolate);
 expectType<any>(chart.xInterpolate());
 expectType<any>(chart.xInterpolate("an argument"));
 
-// has rebound store methods as any-typed methods
+// Has rebound store methods as any-typed methods
 expectType<(...args: any[]) => any>(chart.xTickArguments);
 expectType<any>(chart.xTickArguments());
 expectType<any>(chart.xTickArguments("an argument"));
--- packages/d3fc-chart/test/cartesian.test-d.ts
+++ packages/d3fc-chart/test/cartesian.test-d.ts
@@ -4,29 +4,29 @@ import { CanvasPlotArea, CartesianChart, SvgPlotArea, WebglPlotArea } from '../s
 import { expectType } from 'tsd';
 import { ScaleIdentity, ScaleLinear, scaleTime, scaleLinear } from 'd3';
 
-// can accept zero scale parameters
+// Can accept zero scale parameters
 expectType<CartesianChart<ScaleIdentity, ScaleIdentity>>(chartCartesian());
 
-// can accept one scale parameter
+// Can accept one scale parameter
 expectType<CartesianChart<ScaleLinear<number, number, never>, ScaleIdentity>>(chartCartesian(d3.scaleLinear()));
 
 expectType<CartesianChart<d3.ScaleIdentity<never>, d3.ScaleLinear<number, number, never>>>(chartCartesian(undefined, d3.scaleLinear()));
 
-// can accept two scale parameters
+// Can accept two scale parameters
 expectType<CartesianChart<d3.ScaleLinear<number, number, never>, d3.ScaleTime<number, number, never>>>(chartCartesian(d3.scaleLinear(), scaleTime()));
 
-// can accept a configuration object with one scale
+// Can accept a configuration object with one scale
 expectType<CartesianChart<d3.ScaleIdentity, d3.ScaleLinear<number, number, never>>>(chartCartesian({
     yScale: scaleLinear()
 }));
 
-// can accept a configuration object with both scales
+// Can accept a configuration object with both scales
 expectType<CartesianChart<d3.ScaleLinear<number, number, never>, d3.ScaleLinear<number, number, never>>>(chartCartesian({
     xScale: scaleLinear(),
     yScale: scaleLinear()
 }));
 
-// can accept a configuration object with optional xaxis, yaxis parameters
+// Can accept a configuration object with optional xaxis, yaxis parameters
 expectType<CartesianChart<d3.ScaleLinear<number, number, never>, d3.ScaleLinear<number, number, never>>>(chartCartesian({
     xScale: scaleLinear(),
     yScale: scaleLinear(),
@@ -43,12 +43,12 @@ expectType<CartesianChart<d3.ScaleLinear<number, number, never>, d3.ScaleLinear<
 
 const chart = chartCartesian(d3.scaleLinear(), d3.scaleLinear());
 
-// has rebound scale methods as any-typed methods
+// Has rebound scale methods as any-typed methods
 expectType<(...args: any[]) => any>(chart.xInterpolate);
 expectType<any>(chart.xInterpolate());
 expectType<any>(chart.xInterpolate("an argument"));
 
-// has rebound store methods as any-typed methods
+// Has rebound store methods as any-typed methods
 expectType<(...args: any[]) => any>(chart.xTickArguments);
 expectType<any>(chart.xTickArguments());
 expectType<any>(chart.xTickArguments("an argument"));
